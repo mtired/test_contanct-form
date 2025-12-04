c2b9bf84634db56e9b566024ec06d7ab23289696
mtired <pmmv64947a@gaia.eonet.ne.jp>
2025-12-04 17:56:10 +0900

    ConfirmView作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ConfirmController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ContactController::class, 'index']);
+Route::get('/confirm', [ConfirmController::class, 'index']);
