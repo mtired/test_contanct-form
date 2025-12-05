4f0cb9dd511aac442c71cc1aefecc8866ea5a5ed
mtired <pmmv64947a@gaia.eonet.ne.jp>
2025-12-06 00:30:26 +0900

    ダミーデータ作成

---
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'content' => '1.商品のお届けについて',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'content' => '2.商品の交換について',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'content' => '3.商品トラブル',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'content' => '4.ショップへのお問い合わせ',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'content' => '5.その他',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('categories')->insert($param);
+    }
+}
